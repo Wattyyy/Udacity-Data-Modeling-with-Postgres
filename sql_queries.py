ff313beb9da2cd8d64904fc02ba133d2eb905e87
Wattyyy <yuji.msec.ttasb@gmail.com>
2021-07-19 13:01:31 +0000

    commit for query of users, songs, artists and time table

--- sql_queries.py
+++ sql_queries.py
@@ -65,16 +65,20 @@ songplay_table_insert = ("""
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
 """)
 
 # FIND SONGS
